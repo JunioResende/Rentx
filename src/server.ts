1d58240d239fb6156ede5dcf19bf493bc745c016
JunioResende <junioresende23@gmail.com>
2021-05-31 13:17:36 -0300

    create category

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('🚀 Ignite-Rentx Server its started on port 3333!...');
