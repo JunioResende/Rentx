a34ae179ba4c76cdc5a61045adfeb84e2dca173a
JunioResende <junioresende23@gmail.com>
2021-05-31 12:40:42 -0300

    eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-
+const app = express();
 
 app.listen(3333, () => {
-  console.log('🚀 Ignite-Rentx Server its started on port 3333!...')
-})
+  console.log('🚀 Ignite-Rentx Server its started on port 3333!...');
+});
