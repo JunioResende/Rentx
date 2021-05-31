9c9dec18233e5ecb90f488a67353abd08a6ba9f5
JunioResende <junioresende23@gmail.com>
2021-05-31 12:46:21 -0300

    dubugger config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3333, () => {
   console.log('🚀 Ignite-Rentx Server its started on port 3333!...');
 });
